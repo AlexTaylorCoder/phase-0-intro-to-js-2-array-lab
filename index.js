c429538e8de67c3dd592df61649770d1999ee66d
AlexTaylor <alextaylor515@gmail.com>
2022-04-26 23:42:10 -0400

    commit tree c403a55bd2d9f3b5af751ef0b379c48636c5e2d3

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution here!
+//const createStatsCollector = require("mocha/lib/stats-collector")
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name = "Jeff") {
+    return cats.push(name)
+}
+function destructivelyPrependCat(name = "Todd") {
+    return cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+function prependCat(name = "George") {
+    return [name,...cats]
+}
+function appendCat(name = "Rick") {
+    return [...cats,name]
+}
+function removeLastCat() {
+    return cats.slice(0,2)
+}
+function removeFirstCat() {
+    return cats.slice(1)
+}
